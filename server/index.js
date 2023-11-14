7fbdb7d748513f1b0e6f82b23580314f9464eeca
dragneel <sayakascent@gmail.com>
2023-11-14 19:07:27 +0530

    proxy

--- server/index.js
+++ server/index.js
@@ -9,9 +9,8 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/adid');
   console.log('connected')
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-  
 }
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
